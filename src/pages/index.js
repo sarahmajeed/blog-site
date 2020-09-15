e844783bcbbe5f5517d3cd3aa931997e945e27d2
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-15 15:41:36 +0500

    Basic structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I am Sarah, a full stack developer, living in Karachi, Pakistan</h2>
+    </div>
+  )
 }
+
+export default IndexPage
