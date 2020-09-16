0e5f22e3254bcdbac119e2c72288addbf398071e
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-16 22:56:49 +0500

    Rendering objects from markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,15 @@ module.exports = {
     title: "My blog site",
     author: "Sarah Majeed",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,30 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const blogPosts = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on</p>
+      {blogPosts.allMarkdownRemark.edges.map(blogPost => (
+        <ol>
+          <li>{blogPost.node.frontmatter.title}</li>
+        </ol>
+      ))}
     </Layout>
   )
 }
