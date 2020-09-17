7aac32f9c4157a360830fb5fecd40b22f9d54cc7
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-17 14:09:35 +0500

    generating slug for eac blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 //dynamically generate blog pages
 //node is data structure for storing gatsby data
 
@@ -5,6 +7,12 @@ module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
   // Transform the new node here and create a new node or
   // create a new node field.
-
-  console.log(JSON.stringify(node, undefined, 4))
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
 }
