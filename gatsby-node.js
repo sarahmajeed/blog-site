a0acc1b8e2bcaa084aa6d543bbff3588f71f334b
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-18 15:19:03 +0500

    Dynamically creating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,44 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  //this graphql is a function which returns a promise
+  const { createPage } = actions
+
+  //1. Get path to template
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+  //2. Get markdown data
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  //3. Create new pages
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      //path for this page
+      path: `blog/${edge.node.fields.slug}`,
+      component: blogTemplate,
+      context: {
+        // Add optional context data to be inserted
+        // as props into the page component..
+        //
+        // The context data can also be used as
+        // arguments to the page GraphQL query.
+        //
+        // The page "path" is always available as a GraphQL
+        // argument.
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Blog = () => {
+  return (
+    <div>
+      <Layout>This is blog template</Layout>
+    </div>
+  )
+}
+
+export default Blog
