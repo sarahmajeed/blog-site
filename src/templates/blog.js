1c2fe979769419231b4af74a82e898bf22e725ec
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-19 12:57:00 +0500

    using graphql arguments

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,25 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = props => {
   return (
     <div>
-      <Layout>This is blog template</Layout>
+      <Layout>
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      </Layout>
     </div>
   )
 }
