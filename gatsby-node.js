2ea0ab5f80f19cce7c9c333cc2501e50efa92c74
Sarahmdawood <sarahmajeed368@gmail.com>
2020-10-02 12:28:04 +0500

    :white_check_mark: Integrating contentful in the app part 2

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,45 +3,29 @@ const path = require("path")
 //dynamically generate blog pages
 //node is data structure for storing gatsby data
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-  // Transform the new node here and create a new node or
-  // create a new node field.
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
-
 module.exports.createPages = async ({ graphql, actions }) => {
   //this graphql is a function which returns a promise
   const { createPage } = actions
 
   //1. Get path to template
   const blogTemplate = path.resolve("./src/templates/blog.js")
-  //2. Get markdown data
+  //2. Get markdown data-we need slugs
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
   //3. Create new pages
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       //path for this page
-      path: `blog/${edge.node.fields.slug}`,
+      path: `blog/${edge.node.slug}`,
       component: blogTemplate,
       context: {
         // Add optional context data to be inserted
@@ -52,7 +36,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
         //
         // The page "path" is always available as a GraphQL
         // argument.
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
+//required in order for gatsby parser to find query and run it
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `
@@ -18,11 +20,9 @@ const Blog = props => {
   return (
     <div>
       <Layout>
-        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-        <p>{props.data.markdownRemark.frontmatter.date}</p>
-        <div
-          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-        ></div>
+        <h1>{props.data.contentfulBlogPost.title}</h1>
+        <p>{props.data.contentfulBlogPost.publishedDate}</p>
+        {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
       </Layout>
     </div>
   )
