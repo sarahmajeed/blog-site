2e8c75049ee482409c026e0d630deaa7484fd622
Sarahmdawood <sarahmajeed368@gmail.com>
2020-10-02 11:39:13 +0500

    :white_check_mark: Setting up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
